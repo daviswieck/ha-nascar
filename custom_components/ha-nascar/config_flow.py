3c7be976ca81ebbb758cfce6298e0271484dee05
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2024-05-06 10:10:33 -0500

    Update config_flow.py

--- custom_components/ha-nascar/config_flow.py
+++ custom_components/ha-nascar/config_flow.py
@@ -14,25 +14,15 @@ class haNascarConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        if self._async_current_entries():
-            # If there's already an entry, no need to configure again
-            return self.async_abort(reason="single_instance_allowed")
+        errors = {}
 
-        return self.async_show_form(
-            step_id="confirm",
-            description_placeholders={"domain": DOMAIN},
-        )
-
-    async def async_step_confirm(self, user_input=None):
-        """Handle user confirmation."""
         if user_input is not None:
             # User confirmed, create the config entry
             return self.async_create_entry(
                 title=DOMAIN, data={}
             )
 
-        # User did not confirm, show confirmation form again
         return self.async_show_form(
-            step_id="confirm",
-            description_placeholders={"domain": DOMAIN},
+            step_id="user",
+            errors=errors,
         )
