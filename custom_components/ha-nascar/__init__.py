6da5c570d906379dea402491e10ae1c7d0af4ac8
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2024-05-06 09:54:36 -0500

    Update __init__.py

--- custom_components/ha-nascar/__init__.py
+++ custom_components/ha-nascar/__init__.py
@@ -5,11 +5,11 @@ from homeassistant.helpers import discovery
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = "nascar"
+DOMAIN = "ha_nascar"
 
 async def async_setup(hass, config):
-    """Set up the NASCAR component."""
-    _LOGGER.info("Setting up NASCAR component")
+    """Set up the ha_nascar component."""
+    _LOGGER.info("Setting up ha_nascar component")
     
     # Load the platforms
     for platform in ["sensor"]:
