04f665360c0ca2bbfcf191084e1e4f7d60d2191f
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2025-06-26 16:37:37 -0500

    Update __init__.py

--- custom_components/nascar/__init__.py
+++ custom_components/nascar/__init__.py
@@ -80,5 +80,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    )
     return True
